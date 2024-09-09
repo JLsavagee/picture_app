c69344f71494b48df96e89efbe26e95d91171a8f
Jan-Luca Maiwaldt <janluca@maiwaldt.de>
2024-09-09 22:59:48 +0200

    Feature add manual input fields

--- frontend/src/components/manual.js
+++ frontend/src/components/manual.js
@@ -9,26 +9,35 @@ const Page1 = () => {
             <Sidebar />
             <h1>This is Page "manual"</h1>
             <div className="manual-content">
-                <div className="name-field">
-                    <h1>Name</h1>
-                </div>
-                <div className="surname-field">
-                    <h1>Nachname</h1>
-                </div>
-                <div className="position-field">
-                    <h1>Position</h1>
-                </div>
-                <div className='image-container'>
-                    <div className='image-upload'>
-                        <h1>Image</h1>
+                <form id="manual-upload-form" enctype="multipart/form-data">
+                    <div className="name-field">
+                        <input type="text" name="name" placeholder="Name" /> 
                     </div>
-                    <div className='background-upload'>
-                        <h1>Background</h1>
+                    <div className="surname-field">
+                        <input type="text" name="surname" placeholder="Surname" />
                     </div>
-                </div>
-                <div className="manual-create-button">
-                    <button>Create</button>
-                </div>
+                    <div className="position-field">
+                        <select name="position">
+                            <option value="" disabled selected>Position</option>
+                            <option value="MITTELFELD">MITTELFELD</option>
+                            <option value="ABWEHR">Abwehr</option>
+                            <option value="ANGRIFF">Angriff</option>
+                            <option value="TORWART">Torwart</option>
+                            <option value="TRAINER">Trainer</option>
+                        </select>
+                    </div>
+                    <div className='image-container'>
+                        <div className='image-upload'>
+                            <h1>Image</h1>
+                        </div>
+                        <div className='background-upload'>
+                            <h1>Background</h1>
+                        </div>
+                    </div>
+                    <div className="manual-create-button">
+                        <button>Create</button>
+                    </div>
+                </form>
             </div>
         </div>
     )
