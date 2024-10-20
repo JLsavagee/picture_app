3c3fc9b538e7c989e7a2f117dd1c619c67850fe0
Jan-Luca Maiwaldt <janluca@maiwaldt.de>
2024-10-20 17:02:11 +0200

    add production env to staart script

--- start.sh
+++ start.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 source /home/ubuntu/venv/bin/activate
+
+export $(cat /home/ubuntu/TC-Editor-Backend/backend/.env.production | xargs)
+
 exec /home/ubuntu/venv/bin/gunicorn app:app --bind 0.0.0.0:8000 --workers 3
